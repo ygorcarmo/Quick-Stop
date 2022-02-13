0d58931d11acf43f63be40103fd3d31c3a86bec7
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2022-02-13 20:35:18 +1100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Onboarding from './src/Authentication/Onboarding';
+
+const AuthenticationStack = createNativeStackNavigator();
+
+const AuthenticationNavigator = () =>{
+  return (<AuthenticationStack.Navigator>
+    <AuthenticationStack.Screen  name='Onboarding' component={Onboarding}/>
+  </AuthenticationStack.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthenticationNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
