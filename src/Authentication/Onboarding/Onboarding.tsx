672a4e0bd4c32840eaacb929b743b2f69c9f3e1b
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2022-02-20 17:50:53 +1100

    added title and subtitle for images and now next button also works for android

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import { View, StyleSheet, Dimensions } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import Animated, { color, interpolateColors } from "react-native-reanimated";
+import Animated, { color, interpolateColors, multiply } from "react-native-reanimated";
 import { useValue, onScrollEvent, interpolateColor } from "react-native-redash";
 
 
 
 import Slide, { SLIDE_HEIGHT } from "./Slide";
+import Subslide from "./Subslide";
+
+const BORDER_RADIUS = 75;
 
 const { width } = Dimensions.get("window");
 
@@ -18,21 +21,27 @@ const styles = StyleSheet.create({
     slider: {
         height: SLIDE_HEIGHT,
         backgroundColor: "cyan",
-        borderBottomRightRadius: 75
+        borderBottomRightRadius: BORDER_RADIUS
     },
     footer: {
         flex: 1,
-    }
+    },
+    footerContent: {
+        flexDirection: "row",
+        backgroundColor: "white",
+        borderTopLeftRadius: BORDER_RADIUS
+    },
 });
 
 const slides = [
-    { label: "Relaxed", color: "#BFEAF5" },
-    { label: "Playful", color: "#BEECC4" },
-    { label: "Excentric", color: "#FFE4D9" },
-    { label: "Funky", color: "#FFDDDD" }
+    { title: "Relaxed", subtitle: "Find Your Outfits", description: "Confused about your outfit ? Don't worry. Find the best outfit here!", color: "#BFEAF5" },
+    { title: "Playful", subtitle: "Hear it First, Wear it First", description: "Hating the clothes in your wardroble? Explore hundreds of outfit ideas", color: "#BEECC4" },
+    { title: "Excentric", subtitle: "Your Style, Your Way", description: "Create your indidual & unique style and look amazing everyday", color: "#FFE4D9" },
+    { title: "Funky", subtitle: "Look Good, Feel Good", description: "Discover the latest trends in fashion and empower your personality", color: "#FFDDDD" }
 ];
 
 const Onboarding = () => {
+    const scroll = useRef<null | Animated.ScrollView>(null);
     const x = useValue(0);
     const onScroll = onScrollEvent({ x });
     const backgroundColor = interpolateColors(x, {
@@ -43,6 +52,7 @@ const Onboarding = () => {
         <View style={styles.container}>
             <Animated.View style={[styles.slider, { backgroundColor }]}>
                 <Animated.ScrollView
+                    ref={scroll}
                     horizontal
                     snapToInterval={width}
                     decelerationRate="fast"
@@ -50,15 +60,27 @@ const Onboarding = () => {
                     bounces={false}
                     scrollEventThrottle={1}
                     {...{ onScroll }} >
-                    {slides.map(({ label }, index) => (
-                        <Slide key={index} right={!!(index % 2)} {...{ label }} />
+                    {slides.map(({ title }, index) => (
+                        <Slide key={index} right={!!(index % 2)} {...{ title }} />
                     ))}
                 </Animated.ScrollView>
             </Animated.View>
             <View style={styles.footer}>
                 <Animated.View style={{ ...StyleSheet.absoluteFillObject, backgroundColor }} />
-                <View style={{ flex: 1, backgroundColor: "white", borderTopLeftRadius: 75 }}>
-                </View>
+                <Animated.View style={[styles.footerContent, { width: width * slides.length, flex: 1, transform: [{ translateX: multiply(x, -1) }] }]}>
+                    {slides.map(({ subtitle, description }, index) => (
+                        <Subslide
+                            key={index}
+                            onPress={() => {
+                                console.log("hello")
+                                if (scroll.current) {
+                                    scroll.current.scrollTo({ x: width * (index + 1), animated: true })
+                                }
+                            }}
+                            last={index === (slides.length - 1)}
+                            {...{ subtitle, description }} />
+                    ))}
+                </Animated.View>
             </View>
         </View>
     );
---
+++ src/components/Button.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { GestureHandlerRootView, RectButton } from "react-native-gesture-handler";
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 25,
+        height: 50,
+        width: 245,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    label: {
+        fontSize: 15,
+        fontFamily: "SFProText-Regular",
+        textAlign: "center"
+    }
+})
+
+interface ButtonProps {
+    variant: "default" | "primary";
+    label: string;
+    onPress: () => void;
+};
+
+const Button = ({ variant, label, onPress }: ButtonProps) => {
+    const backgroundColor = variant === "primary" ? "#2CB9B0" : "rgba(12, 13, 52, 0.05)";
+    const color = variant === "primary" ? "white" : "#0C0D34"
+
+    return (
+        <GestureHandlerRootView>
+            <RectButton style={[styles.container, { backgroundColor }]} {...{ onPress }}>
+                <View>
+                    <Text style={[styles.label, { color }]}>{label}</Text>
+                </View>
+            </RectButton>
+        </GestureHandlerRootView>
+    );
+};
+
+Button.defaultProps = { variant: "default" };
+
+export default Button;
