44c6a3ed07e1e4660c2d2b419699fdb165bdea38
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2022-03-09 08:42:12 +1100

    Edited onboarding pages

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,114 +1,233 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, { useRef } from "react";
-import { View, StyleSheet, Dimensions } from "react-native";
-import Animated, { divide, interpolateColors, multiply } from "react-native-reanimated";
-import { useScrollHandler } from "react-native-redash";
+import { View, StyleSheet, Dimensions, Image } from "react-native";
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+} from "react-native-reanimated";
+import { ScrollView } from "react-native-gesture-handler";
+import { interpolateColor } from "react-native-redash";
 
-
+import { useTheme } from "../../components";
+import { AuthNavigationProps } from "../../components/Navigation";
+import { Theme, makeStyles } from "../../components/Theme";
 
 import Slide, { SLIDE_HEIGHT } from "./Slide";
 import Subslide from "./Subslide";
 import Dot from "./Dot";
-import { theme } from "../../components";
-import { AuthenticationRoutes, StackNavigationProps } from "../../components/Navigation";
-
 const { width } = Dimensions.get("window");
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "white"
-    },
-    slider: {
-        height: SLIDE_HEIGHT,
-        backgroundColor: "cyan",
-        borderBottomRightRadius: theme.borderRadii.xl
-    },
-    footer: {
-        flex: 1,
-    },
-    footerContent: {
-        flex: 1,
-        backgroundColor: "white",
-        borderTopLeftRadius: theme.borderRadii.xl
-    },
-    pagination: {
-        ...StyleSheet.absoluteFillObject,
-        flexDirection: "row",
-        height: theme.borderRadii.xl,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-});
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+  },
+  underlay: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    borderBottomRightRadius: theme.borderRadii.xl,
+    overflow: "hidden",
+  },
+  slider: {
+    height: SLIDE_HEIGHT,
+    borderBottomRightRadius: theme.borderRadii.xl,
+  },
+  footer: {
+    flex: 1,
+  },
+  footerContent: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+    borderTopLeftRadius: theme.borderRadii.xl,
+  },
+  pagination: {
+    ...StyleSheet.absoluteFillObject,
+    height: theme.borderRadii.xl,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
 
-const slides = [
-    { title: "Relaxed", subtitle: "Find Your Outfits", description: "Confused about your outfit ? Don't worry. Find the best outfit here!", color: "#BFEAF5" },
-    { title: "Playful", subtitle: "Hear it First, Wear it First", description: "Hating the clothes in your wardroble? Explore hundreds of outfit ideas", color: "#BEECC4" },
-    { title: "Excentric", subtitle: "Your Style, Your Way", description: "Create your indidual & unique style and look amazing everyday", color: "#FFE4D9" },
-    { title: "Funky", subtitle: "Look Good, Feel Good", description: "Discover the latest trends in fashion and empower your personality", color: "#FFDDDD" }
+interface Slide {
+  title: string;
+  subtitle: string;
+  description: string;
+  color: string;
+  picture: {
+    width: number;
+    height: number;
+    src: number;
+  };
+}
+
+export const slides: Slide[] = [
+  {
+    title: "Relaxed",
+    subtitle: "Find Your Outfits",
+    description:
+      "Confused about your outfit? Don’t worry! Find the best outfit here!",
+    color: "#BFEAF5",
+    picture: {
+      src: require("../assets/1.png"),
+      width: 2513,
+      height: 3583,
+    },
+  },
+  {
+    title: "Playful",
+    subtitle: "Hear it First, Wear it First",
+    description:
+      "Hating the clothes in your wardrobe? Explore hundreds of outfit ideas",
+    color: "#BEECC4",
+    picture: {
+      src: require("../assets/2.png"),
+      width: 2791,
+      height: 3744,
+    },
+  },
+  {
+    title: "Excentric",
+    subtitle: "Your Style, Your Way",
+    description:
+      " Create your individual & unique style and look amazing everyday",
+    color: "#FFE4D9",
+    picture: {
+      src: require("../assets/3.png"),
+      width: 2738,
+      height: 3244,
+    },
+  },
+  {
+    title: "Funky",
+    subtitle: "Look Good, Feel Good",
+    description:
+      "Discover the latest trends in fashion and explore your personality",
+    color: "#FFDDDD",
+    picture: {
+      src: require("../assets/4.png"),
+      width: 1757,
+      height: 2551,
+    },
+  },
 ];
+export const assets = slides.map((slide) => slide.picture.src);
 
-const Onboarding = ({ navigation }: StackNavigationProps<AuthenticationRoutes, "Onboarding">) => {
-    const scroll = useRef<null | Animated.ScrollView>(null);
-    // const x = useValue(0);
-    const { scrollHandler, x } = useScrollHandler();
-    const backgroundColor = interpolateColors(x, {
-        inputRange: slides.map((_, i) => i * width),
-        outputColorRange: slides.map(slide => slide.color)
-    });
-    return (
-        <View style={styles.container}>
-            <Animated.View style={[styles.slider, { backgroundColor }]}>
-                <Animated.ScrollView
-                    ref={scroll}
-                    horizontal
-                    snapToInterval={width}
-                    decelerationRate="fast"
-                    showsHorizontalScrollIndicator={false}
-                    bounces={false}
-                    {...scrollHandler} >
-                    {slides.map(({ title }, index) => (
-                        <Slide key={index} right={!!(index % 2)} {...{ title }} />
-                    ))}
-                </Animated.ScrollView>
+const Onboarding = ({ navigation }: AuthNavigationProps<"Onboarding">) => {
+  const styles = useStyles();
+  const theme = useTheme();
+  const scroll = useRef<Animated.ScrollView>(null);
+  const x = useSharedValue(0);
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: ({ contentOffset }) => {
+      x.value = contentOffset.x;
+    },
+  });
+  const backgroundColor = useDerivedValue(() =>
+    interpolateColor(
+      x.value,
+      slides.map((_, i) => i * width),
+      slides.map((slide) => slide.color)
+    )
+  );
+  const slider = useAnimatedStyle(() => ({
+    backgroundColor: backgroundColor.value,
+  }));
+  const background = useAnimatedStyle(() => ({
+    backgroundColor: backgroundColor.value,
+  }));
+  const currentIndex = useDerivedValue(() => x.value / width);
+  const footerStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: -x.value }],
+  }));
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.slider, slider]}>
+        {slides.map(({ picture }, index) => {
+          const style = useAnimatedStyle(() => ({
+            opacity: interpolate(
+              x.value,
+              [(index - 0.5) * width, index * width, (index + 0.5) * width],
+              [0, 1, 0],
+              Extrapolate.CLAMP
+            ),
+          }));
+          return (
+            <Animated.View style={[styles.underlay, style]} key={index}>
+              <Image
+                source={picture.src}
+                style={{
+                  width: width - theme.borderRadii.xl,
+                  height:
+                    ((width - theme.borderRadii.xl) * picture.height) /
+                    picture.width,
+                }}
+              />
             </Animated.View>
-            <View style={styles.footer}>
-                <Animated.View style={{ ...StyleSheet.absoluteFillObject, backgroundColor }} />
-                <View style={styles.footerContent}>
-                    <View style={styles.pagination}>
-                        {slides.map((_, index) => (<Dot key={index}
-                            currentIndex={divide(x, width)}
-                            {...{ index }} />))}
-                    </View>
-                    <Animated.View style={{
-                        flex: 1,
-                        flexDirection: "row",
-                        width: width * slides.length,
-                        transform: [{ translateX: multiply(x, -1) }]
-                    }}>
-
-                        {slides.map(({ subtitle, description }, index) => {
-                            const last = index === (slides.length - 1);
-
-                            return (
-                                (
-                                    <Subslide
-                                        key={index}
-                                        onPress={() => {
-                                            if (last)
-                                                navigation.navigate("Welcome");
-                                            else
-                                                scroll?.current.scrollTo({ x: width * (index + 1), animated: true })
-
-                                        }}
-                                        {...{ subtitle, description, last }} />
-                                )
-                            )
-                        })}
-                    </Animated.View>
-                </View>
-            </View>
+          );
+        })}
+        <Animated.ScrollView
+          ref={scroll}
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          onScroll={onScroll}
+          scrollEventThrottle={16}
+        >
+          {slides.map(({ title, picture }, index) => (
+            <Slide key={index} right={!!(index % 2)} {...{ title, picture }} />
+          ))}
+        </Animated.ScrollView>
+      </Animated.View>
+      <View style={styles.footer}>
+        <Animated.View style={[StyleSheet.absoluteFill, background]} />
+        <View style={styles.footerContent}>
+          <View style={styles.pagination}>
+            {slides.map((_, index) => (
+              <Dot key={index} currentIndex={currentIndex} {...{ index }} />
+            ))}
+          </View>
+          <Animated.View
+            style={[
+              {
+                flex: 1,
+                flexDirection: "row",
+                width: width * slides.length,
+              },
+              footerStyle,
+            ]}
+          >
+            {slides.map(({ subtitle, description }, index) => {
+              const last = index === slides.length - 1;
+              return (
+                <Subslide
+                  key={index}
+                  onPress={() => {
+                    if (last) {
+                      navigation.navigate("Welcome");
+                    } else {
+                      scroll.current?.scrollTo({
+                        x: width * (index + 1),
+                        animated: true,
+                      });
+                    }
+                  }}
+                  {...{ subtitle, description, last }}
+                />
+              );
+            })}
+          </Animated.View>
         </View>
-    );
+      </View>
+    </View>
+  );
 };
 
 export default Onboarding;
--- src/Authentication/Onboarding/Subslide.tsx
+++ src/Authentication/Onboarding/Subslide.tsx
@@ -1,42 +1,49 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Button, Text } from '../../components';
+import React from "react";
+import { View, StyleSheet } from "react-native";
 
+import { Button, Text } from "../../components";
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 24,
-        flex: 1,
-    },
-    subtitle: {
-        marginBottom: 12,
-        textAlign: "center"
-    },
-    description: {
-        textAlign: "center",
-        marginBottom: 40,
-    }
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 44,
+    flex: 1,
+  },
+  subtitle: {
+    marginBottom: 12,
+    textAlign: "center",
+  },
+  description: {
+    color: "#0C0D34",
+    textAlign: "center",
+    marginBottom: 40,
+  },
 });
 
 interface SubslideProps {
-    subtitle: string;
-    description: string;
-    last?: boolean;
-    onPress: () => void;
-};
+  subtitle: string;
+  description: string;
+  last?: boolean;
+  onPress: () => void;
+}
 
 const Subslide = ({ subtitle, description, last, onPress }: SubslideProps) => {
-    return (
-        <View style={styles.container}>
-            <Text variant="title2" style={styles.subtitle} >{subtitle}</Text>
-            <Text variant="body" style={styles.description}>{description}</Text>
-            <Button label={last ? "Let's get started" : "Next"}
-                variant={last ? "primary" : "default"}
-                {...{ onPress }} />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text variant="title2" style={styles.subtitle}>
+        {subtitle}
+      </Text>
+      <Text variant="body" style={styles.description}>
+        {description}
+      </Text>
+      <Button
+        label={last ? "Let's get started" : "Next"}
+        variant={last ? "primary" : "default"}
+        {...{ onPress }}
+      />
+    </View>
+  );
 };
 
 export default Subslide;
