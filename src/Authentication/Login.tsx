68e366529022d35802625ed73c97d190a69bfebb
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2022-03-09 08:49:03 +1100

    edited index and login page on authentication

--- src/Authentication/Login.tsx
+++ src/Authentication/Login.tsx
@@ -13,109 +13,109 @@ import Checkbox from "../components/Form/Checkbox";
 import Footer from "./components/Footer";
 
 const LoginSchema = Yup.object().shape({
-    password: Yup.string()
-        .min(2, "Too Short!")
-        .max(50, "Too Long!")
-        .required("Required"),
-    email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
 });
 
 const Login = ({ navigation }: AuthNavigationProps<"Login">) => {
-    const {
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        errors,
-        touched,
-        values,
-        setFieldValue,
-    } = useFormik({
-        validationSchema: LoginSchema,
-        initialValues: { email: "", password: "", remember: true },
-        onSubmit: () =>
-            navigation.dispatch(
-                CommonActions.reset({
-                    index: 0,
-                    routes: [{ name: "Home" }],
-                })
-            ),
-    });
-    const password = useRef<RNTextInput>(null);
-    const footer = (
-        <Footer
-            title="Don’t have an account?"
-            action="Sign Up here"
-            onPress={() => navigation.navigate("SignUp")}
+  const {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    errors,
+    touched,
+    values,
+    setFieldValue,
+  } = useFormik({
+    validationSchema: LoginSchema,
+    initialValues: { email: "", password: "", remember: true },
+    onSubmit: () =>
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [{ name: "Home" }],
+        })
+      ),
+  });
+  const password = useRef<RNTextInput>(null);
+  const footer = (
+    <Footer
+      title="Don’t have an account?"
+      action="Sign Up here"
+      onPress={() => navigation.navigate("SignUp")}
+    />
+  );
+  return (
+    <Container pattern={0} {...{ footer }}>
+      <Text variant="title1" textAlign="center" marginBottom="l">
+        Welcome back
+      </Text>
+      <Text variant="body" textAlign="center" marginBottom="l">
+        Use your credentials below and login to your account
+      </Text>
+      <Box>
+        <Box marginBottom="m">
+          <TextInput
+            icon="mail"
+            placeholder="Enter your Email"
+            onChangeText={handleChange("email")}
+            onBlur={handleBlur("email")}
+            error={errors.email}
+            touched={touched.email}
+            autoCapitalize="none"
+            autoCompleteType="email"
+            returnKeyType="next"
+            returnKeyLabel="next"
+            onSubmitEditing={() => password.current?.focus()}
+          />
+        </Box>
+        <TextInput
+          ref={password}
+          icon="lock"
+          placeholder="Enter your Password"
+          onChangeText={handleChange("password")}
+          onBlur={handleBlur("password")}
+          error={errors.password}
+          touched={touched.password}
+          autoCompleteType="password"
+          autoCapitalize="none"
+          returnKeyType="go"
+          returnKeyLabel="go"
+          onSubmitEditing={() => handleSubmit()}
+          secureTextEntry
         />
-    );
-    return (
-        <Container pattern={0} {...{ footer }}>
-            <Text variant="title1" textAlign="center" marginBottom="l">
-                Welcome back
+        <Box
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          marginVertical="s"
+        >
+          <Checkbox
+            label="Remember me"
+            checked={values.remember}
+            onChange={() => setFieldValue("remember", !values.remember)}
+          />
+          <BorderlessButton
+            onPress={() => navigation.navigate("ForgotPassword")}
+          >
+            <Text variant="button" color="primary">
+              Forgot password
             </Text>
-            <Text variant="body" textAlign="center" marginBottom="l">
-                Use your credentials below and login to your account
-            </Text>
-            <Box>
-                <Box marginBottom="m">
-                    <TextInput
-                        icon="mail"
-                        placeholder="Enter your Email"
-                        onChangeText={handleChange("email")}
-                        onBlur={handleBlur("email")}
-                        error={errors.email}
-                        touched={touched.email}
-                        autoCapitalize="none"
-                        autoCompleteType="email"
-                        returnKeyType="next"
-                        returnKeyLabel="next"
-                        onSubmitEditing={() => password.current?.focus()}
-                    />
-                </Box>
-                <TextInput
-                    ref={password}
-                    icon="lock"
-                    placeholder="Enter your Password"
-                    onChangeText={handleChange("password")}
-                    onBlur={handleBlur("password")}
-                    error={errors.password}
-                    touched={touched.password}
-                    autoCompleteType="password"
-                    autoCapitalize="none"
-                    returnKeyType="go"
-                    returnKeyLabel="go"
-                    onSubmitEditing={() => handleSubmit()}
-                    secureTextEntry
-                />
-                <Box
-                    flexDirection="row"
-                    justifyContent="space-between"
-                    alignItems="center"
-                    marginVertical="s"
-                >
-                    <Checkbox
-                        label="Remember me"
-                        checked={values.remember}
-                        onChange={() => setFieldValue("remember", !values.remember)}
-                    />
-                    <BorderlessButton
-                        onPress={() => navigation.navigate("ForgotPassword")}
-                    >
-                        <Text variant="button" color="primary">
-                            Forgot password
-                        </Text>
-                    </BorderlessButton>
-                </Box>
-                <Box alignItems="center" marginTop="m">
-                    <Button
-                        variant="primary"
-                        onPress={handleSubmit}
-                        label="Log into your account"
-                    />
-                </Box>
-            </Box>
-        </Container>
-    );
+          </BorderlessButton>
+        </Box>
+        <Box alignItems="center" marginTop="m">
+          <Button
+            variant="primary"
+            onPress={handleSubmit}
+            label="Log into your account"
+          />
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Login;
--- src/Authentication/index.tsx
+++ src/Authentication/index.tsx
@@ -1,21 +1,32 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Welcome, { assets as WelcomeAssets } from "./Welcome";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import { AuthenticationRoutes } from "../components/Navigation";
-import Onboarding from "./Onboarding";
-import Login from './Login';
 
-export const assets = [...WelcomeAssets]
+import Onboarding, { assets as onBoardingAssets } from "./Onboarding";
+import Welcome, { assets as welcomeAssets } from "./Welcome";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import ForgotPassword from "./ForgotPassword";
+import PasswordChanged from "./PasswordChanged";
+export const assets = [...onBoardingAssets, ...welcomeAssets];
 
-const AuthenticationStack = createNativeStackNavigator<AuthenticationRoutes>();
-
+const AuthenticationStack = createStackNavigator<AuthenticationRoutes>();
 export const AuthenticationNavigator = () => {
   return (
-    <AuthenticationStack.Navigator screenOptions={{ headerShown: false }}>
-      <AuthenticationStack.Screen name='Onboarding' component={Onboarding} />
-      <AuthenticationStack.Screen name='Welcome' component={Welcome} />
-      <AuthenticationStack.Screen name='Login' component={Login} />
+    <AuthenticationStack.Navigator headerMode="none">
+      <AuthenticationStack.Screen name="Onboarding" component={Onboarding} />
+      <AuthenticationStack.Screen name="Welcome" component={Welcome} />
+      <AuthenticationStack.Screen name="Login" component={Login} />
+      <AuthenticationStack.Screen name="SignUp" component={SignUp} />
+      <AuthenticationStack.Screen
+        name="ForgotPassword"
+        component={ForgotPassword}
+      />
+      <AuthenticationStack.Screen
+        name="PasswordChanged"
+        component={PasswordChanged}
+      />
     </AuthenticationStack.Navigator>
   );
 };
-
